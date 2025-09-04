709ddb65e231f7beca168c38df8aa44055ca7ed6
dylan yagninim <dylanmagnym@gmail.com>
2025-09-04 15:13:38 -0400

    template for the code

---
+++ lec2/Program.cs
@@ -0,0 +1,19 @@
+﻿public class StudentGroup
+{
+    private string groupName;
+    private string[] studentNames;
+    private int numberOfStudents;
+
+    public StudentGroup(string nameOfGroup, int fixedNumberOfStudents)
+    { }
+
+    public string getStudent(int studentIndex)
+    { return studentNames[studentIndex]; }
+    public void addStudent(string nameOfStu, int fixedNumberOfStudents)
+    { }
+
+    public void displayGroup()
+    { }
+
+
+ }
